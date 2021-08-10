81daf70ee50e72f74f8b431bc5bdb1088db55f31
rojajullapalli <rojajules@gmail.com>
2021-08-10 12:00:25 +0530

    welcome to employee builder program

---
+++ src/com/roja/wage/EmpWageBuilder.java
@@ -0,0 +1,14 @@
+package com.roja.wage;
+/**
+ *EmpWageBuildwer is an application to welcome.
+ *
+ * @author : roja
+ * @version :1.0
+ * @since :10-08-2021
+
+ **/
+public class EmpWageBuilder {
+    public static void main(String[] args) {
+        System.out.println("welcome to employee wage builder");
+    }
+}
