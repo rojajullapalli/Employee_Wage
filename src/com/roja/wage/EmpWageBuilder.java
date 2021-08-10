c4e6697fdf715dec41b9b588cb9986f670c48de6
rojajullapalli <rojajules@gmail.com>
2021-08-10 12:02:51 +0530

    checking whether employee is present or absent

--- src/com/roja/wage/EmpWageBuilder.java
+++ src/com/roja/wage/EmpWageBuilder.java
@@ -1,6 +1,6 @@
 package com.roja.wage;
 /**
- *EmpWageBuildwer is an application to welcome.
+ *EmpWageBuildweUC1 is an application to check whether Employee is present or not.
  *
  * @author : roja
  * @version :1.0
@@ -9,6 +9,19 @@ package com.roja.wage;
  **/
 public class EmpWageBuilder {
     public static void main(String[] args) {
-        System.out.println("welcome to employee wage builder");
+        //constant to check employee is full time
+        int IS_FULL_TIME = 1;
+        //it will generate the random number between 0 and 1 and stores it in empcheck
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        /**
+         * if empcheck equal to 1 then it goes to if and print employee is present .
+         *if empcheck is 0 then goes to else employee is absent.
+         **/
+        if (empcheck ==  IS_FULL_TIME) {
+            System.out.println("Employee is present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }
 }
